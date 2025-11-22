9b0268493544d8297e0dadad27b966d0b34627b5
NANTETU <masaki741852963@gmail.com>
2025-11-22 14:09:22 +0900

    styleが適応されるように

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App.jsx'; // 修正: 親ディレクトリを示す'../'を削除し、'./'で同じディレクトリを参照する
 
 // アプリケーションをpublic/index.htmlのid="root"要素にレンダリングする
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+﻿// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // 💡 CRAの標準的なフォルダ構成をカバーする設定
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
