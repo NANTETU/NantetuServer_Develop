e2213e2293a7b906f139404d138056b6ac71be25
NANTETU <masaki741852963@gmail.com>
2025-12-24 13:45:39 +0900

    zｗ

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,2 @@
 export { Navbar } from './Navbar';
 export { Footer } from './Footer';
-export { AIChat } from './AIChat';
-export * from './UI';
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,3 @@ export { CommandsPage } from './CommandsPage';
 export { TermsPage } from './TermsPage';
 export { PrivacyPage } from './PrivacyPage';
 export { NotFoundPage } from './NotFoundPage';
-
-// New extractions
-export { default as HomePage, JoinPage } from './Home';
-export { NewsPage, NewsDetail } from './NewsPage';
-export { ArticlesPage, ArticleDetail } from './ArticlesPage';
-export { ProfilePage } from './ProfilePage';
-export { ProfileEditPage } from './ProfileEditPage';
-export { AdminPage } from './AdminPage';
-export { SearchResultsPage } from './SearchResultsPage';
